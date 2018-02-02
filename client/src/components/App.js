d2b6deef54ef8674d6d43a40bd41c189a8f55b1e
ocin-rye <contact@nicobryan.com>
2018-02-02 11:52:01 -0500

    Added react client and mongoose user model

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div> React is working! </div>
+    );
+  }
+}
+
+export default App;
