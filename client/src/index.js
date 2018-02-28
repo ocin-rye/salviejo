9d3e42dddaf7d12724ba29d422bdf26ca138d36d
ocin-rye <contact@nicobryan.com>
2018-02-28 12:05:36 -0500

    Whitelist changed to comment

--- client/src/index.js
+++ client/src/index.js
@@ -17,7 +17,7 @@ const persistConfig = {
   key: 'root',
   storage,
   stateReconciler: hardSet,
-  whitelist: ['cart'],
+  // whitelist: ['cart'],
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers);
