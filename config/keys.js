a0efbef42e7c98ae62383f6ff69a0adc8a323a7b
ocin-rye <contact@nicobryan.com>
2018-03-09 19:34:30 -0500

    Updated webpack prod config. 6th

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,10 @@
-const prod = require('./prod');
-const dev = require('./dev');
+// const prod = require('./prod');
+// const dev = require('./dev');
 
 if (process.env.NODE_ENV === 'production') {
+  const prod = require('./prod');
   module.exports = prod;
 } else {
+  const dev = require('./dev');
   module.exports = dev;
 }
